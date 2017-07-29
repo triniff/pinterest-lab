2f091aec6364122f16bc432ea8fe15a35c8d087d
Trinidad Fuentes <tfuentesfuentealba@gmail.com>
2017-07-28 23:34:20 -0400

    agregando estilos a modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-	var titulo, descrp, user, imagen, hashtag, modal, modalBody;
+	var titulo, descrp, user, imagen, hashtag, modal, modalBody, iconos;
 	for (var i = 0; i < 20; i++) {
 		titulo = '<h1>' + data[i].title + '</h1>';
 		descrp = '<p>'+ data[i].description +'</p>';
-		userHash = '<div><h4>' + data[i].user + '</h4><a href="#">' + data[i].hashtag+ '</a></div>';
+		iconos = '<div class="iconos"><i class="fa fa-upload" aria-hidden="true"></i><i class="fa fa-check" aria-hidden="true"></i><i class="fa fa-ellipsis-h" aria-hidden="true"></i><button type="button" class="btn btn-default btn-save"><i class="fa fa-thumb-tack" aria-hidden="true"></i> Guardar</button></div>';
+		userHash = '<div class="hashtag-user"><h6>M</h6><h4>' + data[i].user + '</h4><a href="#">#' + data[i].hashtag + '</a></div>';
 		imagen = '<img src="dist/img/'+ data[i].image_url+'">';
 
 		$('.cont-img').append('<div class="item-pint" data-toggle="modal" data-target="#'+ data[i].id +'">'+ imagen +'<div class="text-desc">'+ titulo + descrp + userHash +'</div></div>');
-		modalBody =  '<div class="modal-body">' + imagen + titulo + descrp + '</div>';
-		modal = '<div class="modal fade" id="'+ data[i].id +'" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><i class="fa fa-upload" aria-hidden="true"></i><i class="fa fa-check" aria-hidden="true"></i><i class="fa fa-ellipsis-h" aria-hidden="true"></i><button type="button" class="btn btn-default"><i class="fa fa-thumb-tack" aria-hidden="true"></i> Guardar</button></div>'+ modalBody+'</div></div></div>';
+		modalBody =  '<div class="modal-body">' + titulo + imagen + iconos + userHash + '<button type="button" class="btn btn-default btn-read">Leerlo</button>' + descrp + '</div>';
+		modal = '<div class="modal fade" id="'+ data[i].id +'" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">' + iconos + '</div>'+ modalBody+'</div></div></div>';
+		$('.modalBody>.hashtag-user').append('Holi');
 		$('.cont-img').append(modal);
 	}
 
